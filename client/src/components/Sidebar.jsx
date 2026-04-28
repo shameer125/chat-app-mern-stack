b8a36f1cae2521808033cb8c6a61046a08b7f73d
shameer125 <alishameer251@gmail.com>
2026-04-27 19:54:13 -0700

    optimize sidebar.jsx component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,10 +13,9 @@ const Sidebar = () => {
 
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
-
   const { logout, onlineUser } = useContext(AuthContext);
-
   const [input, setInput] = useState(false);
+  
 
   const filteredUsers = input ? users.filter((user) =>
     user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
@@ -111,7 +110,7 @@ const Sidebar = () => {
             {unseenMessages?.[user._id] > 0 && (
               <p
                 className="absolute top-4 right-4 text-xs
-                    h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50"
+                h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50"
               >
                 {unseenMessages[user._id]}
               </p>
