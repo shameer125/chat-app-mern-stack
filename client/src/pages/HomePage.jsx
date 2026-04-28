279afed3d1cd0d38466e357ec4b666f027b95186
shameer125 <alishameer251@gmail.com>
2026-04-27 19:59:01 -0700

    some changes in home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import { ChatContext } from '../context/ChatContext'
 
 const HomePage = () => {
 
-    const {selectedUser} = useContext(ChatContext)
+const {selectedUser} = useContext(ChatContext)
 
   return (
       <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]' >
@@ -18,6 +18,7 @@ const HomePage = () => {
               <ChatContainer />
               <RightSidebar />
           </div>
+          
     </div>
   )
 }
