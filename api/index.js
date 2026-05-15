25756ffe7cc2cba02956d58363a0c0b4169cdcf2
shameer125 <alishameer251@gmail.com>
2026-05-15 16:27:28 -0700

    update index.js

--- api/index.js
+++ api/index.js
@@ -1,32 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(
   cors({
-    origin: "*", // temporarily for testing
+    origin: "*",
     credentials: true,
   }),
 );
 
 app.use(express.json());
 
-// connect MongoDB (SAFE for Vercel)
-if (!mongoose.connection.readyState) {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log(err));
-}
-
-// test route
+// TEST ROUTE ONLY
 app.get("/", (req, res) => {
-  res.send("API Working");
+  res.send("API WORKING");
 });
-
-// routes
-app.use("/api/users", require("../server/routes/userRoutes"));
 
 module.exports = app;
