c6ca9126aead2b98550219c941470febab006093
shameer125 <alishameer251@gmail.com>
2026-05-15 16:34:50 -0700

    remove vercel.json for test

--- api/index.js
+++ api/index.js
@@ -1,18 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  }),
-);
-
-app.use(express.json());
-
-// TEST ROUTE ONLY
 app.get("/", (req, res) => {
   res.send("API WORKING");
 });
